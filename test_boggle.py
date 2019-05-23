a804731689ed62e1c9f15feeec2ce3b21aa14c4a
Sabelo Mcanyana <sabelom@gmail.com>
2019-05-23 22:59:18 +0000

    Created test for creating empty grid

--- test_boggle.py
+++ test_boggle.py
@@ -1,5 +1,15 @@
 import unittest
 
-class test_boggle(unittest.TestCase):
- def test_is_this_thing_on(self):
-        self.assertEqual(1, 1)
+import boggle
+
+class TestBoggle(unittest.TestCase):
+    """
+    Our test suite for boggle solver
+    """
+
+    def test_can_create_an_empty_grid(self):
+        """
+        Test to see if we can create an empty grid
+        """
+        grid = boggle.make_grid(0, 0)
+        self.assertEqual(len(grid), 0)
